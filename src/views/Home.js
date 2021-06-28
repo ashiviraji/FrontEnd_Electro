96c699136456ec4e6493bb80ae8b8064f5ab6010
MadhushankaUCSC <buthsaramadhushanka@gmail.com>
2021-06-28 13:12:22 +0530

    added basic files

---
+++ src/views/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../assets/css/home.css';
+export default function Home() {
+    return (
+
+        <div className="">
+            <h1>home</h1>
+
+        </div>
+    )
+}
