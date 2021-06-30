4a7120ab24bf2a1e52f9a732a9b4171195069e9c
hasinihatharasinghe97 <hasinividushanka@gmail.com>
2021-07-01 01:10:14 +0530

    CEB Engineer

---
+++ src/components/common/Navbar/NavabarElement.js
@@ -0,0 +1,125 @@
+
+import { Link as LinkR } from 'react-router-dom';
+import { Link as LinkS } from 'react-scroll'
+import styled from 'styled-components';
+export const Nav = styled.nav`
+background:#23e694;
+height:80px;
+display:flex;
+justify-content:center;
+align-items:center;
+font-size:1rem;
+position:sticky;
+top:0;
+margin-right:0px;
+z-index:10;
+@media screen and (max-width:960px){
+    transition:0.8s all ease;
+}
+`
+
+export const NavbarContainer = styled.div`
+display:flex;
+justify-content:space-between;
+height:80px;
+z-index:1;
+width:100%;
+
+
+margin-left: 120px;
+
+`
+export const NavLogo = styled(LinkR)`
+color:#fff;
+justify-self:flex-start;
+cursor:pointer;
+display:flex;
+font-size:1.5rem;
+align:item:left;
+margin-left:10px;
+font-weight:bold;
+margin-top:15px;
+text-decoration:none;
+@media screen and (max-width:980px){
+    margin-left:-100px;
+}
+`
+export const MobileIcon = styled.div`
+display:none;
+
+@media screen and (max-width:818px){
+    display:block;
+    position:absolute;
+    margin-top:-20px;
+    right:0;
+    transform:translate(-100%,60%);
+    font-size:1.9rem;
+    cursor:pointer;
+    color:white;
+   
+}
+
+`
+export const NavMenu = styled.ul`
+    display:flex;
+    text-align: center;;
+	list-style: none;
+	align-items: center;
+	margin-right: -12px;
+
+	@media screen and (max-width: 818px){
+		display: none;
+	}
+
+
+`
+export const NavItem = styled.li`
+   height:80px;
+
+`
+
+export const NavLinks = styled(LinkS)`
+color: #fff;
+	display: flex;
+    margin-right:50px;
+	align-items: center;
+	text-decoration: none;
+	padding: 0 1rem;
+	height: 100%;
+	cursor: pointer;
+	&.active{
+		border-bottom: 3px solid #01bf71;
+	}
+`
+export const NavBtn = styled.nav`
+display:flex;
+align-items:center;
+@media screen and (max-width: 818px){
+		display: none;
+	}
+`
+export const NavBtnLinks = styled(LinkR)`
+color:#01bf71;
+border-radius:50px;
+background:#fff;
+white-space:nowrap;
+padding:6px 20px;
+outline:none;
+border:none;
+transition:all 0.2s ease-in-out;
+text-decoration:none;
+cursor:pointer;
+font-size:16px;
+align:item:center;
+font-weight:bold;
+ margin-right:45px;
+ margin-top:23px;
+ display:flex;
+ box-shadow: 5px 8px 8px #01bf71;
+&:hover{
+    transition:all 0.2s ease-in-out;
+    background:#01bf71;
+    color:#fff;
+}
+
+`
--- src/views/Contactus.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Contactus() {
-    return (
-        <div className="container my-6">
-            <h1>contactus</h1>
-        </div>
-    )
-}
