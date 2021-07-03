14f5f4e420829113a8ac5489c7fa99077935f409
TharinduWithanage <56493297+TharinduWithanage@users.noreply.github.com>
2021-07-03 12:54:08 +0530

    modified calculating bill

--- src/views/customer/CalculateBillForm.js
+++ src/views/customer/CalculateBillForm.js
@@ -1,5 +1,14 @@
 import React, {useState, useEffect} from 'react'
-import { Grid, TextField } from '@material-ui/core';
+import { Grid, makeStyles, TextField } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .MuiFormControl-root": {
+      width: "80%",
+      margin: theme.spacing(1),
+    },
+  },
+}));
 
 const initialFvalues = {
     id : 0,
@@ -17,27 +26,38 @@ const initialFvalues = {
 export default function CalculateBillForm() {
 
     const [values, setValues] = useState(initialFvalues);
+    const classes = useStyles();
 
     return (
         <div>
             <h1> Hi babala </h1>
-            <form>
+            <form className={classes.root}>
             
             <Grid container>
                 <Grid item xs={6}>
+                
                     <TextField
-                        TextField id="outlined-basic" 
-                        label="Outlined" 
-                        variant="outlined"
+                        TextField id="standard-basic" 
+                        label="Full Name"
                         value={values.fullName}
                     />
                     <TextField
-                        TextField id="outlined-basic" 
-                        label="Outlined" 
-                        variant="outlined"
+                        TextField id="standard-basic" 
+                        label="Email"
                         value={values.email}
                     />
                 </Grid>
+                <Grid item xs={6}>
+                    <TextField id="standard-basic" label="Standard"  />
+
+                    <TextField
+                        id="standard-number"
+                        label="Number"
+                        type="number"
+                        InputLabelProps={{shrink: true,}}
+                    />
+                </Grid>
+                
             </Grid>
                            
             </form>
