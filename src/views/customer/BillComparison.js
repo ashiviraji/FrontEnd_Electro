489f277ba50456e318ce5f2c2529cf41866e49f8
Minuri427 <yasarawickramanayaka@gmail.com>
2021-07-04 21:51:13 +0530

    bill-comparison final

--- src/views/customer/BillComparison.js
+++ src/views/customer/BillComparison.js
@@ -7,18 +7,17 @@ import { Link } from "react-router-dom";
 
 export default function billcomparison() {
   return (
-    <div className="frm">
-      <div className="grp">
-        <div className="main-title">
-         <h4 className="text-center"> Bill Comparison</h4>
+    <div className="frm-bill">
+      <div className="grp-bill">
+        <div className="text-center main-title ">
+         <h4> Bill Comparison</h4>
         </div>
 
         <div className="row">
           <div className="col-sm-6">
             <div className="card1 card border-success mb-3">
-              <div className="card-body">
-                <h5 className="card-title">TOU MODEL</h5>
-                {/* <p className="card-text">LKR 4590</p> */}
+              <div className="card-body tou">
+                <h5 className="card-title text-center">TOU MODEL</h5>
                 <div className="form-group">
                   <input
                     type="text"
@@ -34,9 +33,8 @@ export default function billcomparison() {
           </div>
           <div className="col-sm-6">
             <div className="card2">
-              <div className="card-body">
-                <h5 className="card-title">FIXED MODEL</h5>
-                {/* <p className="card-text">LKR 3320</p> */}
+              <div className="card-body fixed">
+                <h5 className="card-title text-center">FIXED MODEL</h5>
                 <div className="form-group">
                   <input
                     type="text"
@@ -52,13 +50,13 @@ export default function billcomparison() {
           </div>
         </div>
         <div className="card text-center best-model">
-          <div className="card-body">
+          <div className="card-body best">
           <h5> Best Model : Fixed </h5>
           </div>
         </div>
         
-        <div className="card text-center usage-device">
-          <div className="card-body">
+        <div className="card text-center usage">
+          <div className="card-body usage-of-device">
             <h5 className="card-title">Usage of Device Wise</h5>
             
             <Link className="btn btn-success btn-lg btn-tou" to="#">TOU Model</Link>
@@ -68,10 +66,10 @@ export default function billcomparison() {
           </div>
         </div>
 
-        <div>
+        <div className="text-center">
           <h5> View TOU Suggestions</h5>
           
-          <Link className="btn btn-info btn-lg btn-suggest" to="#"><FaThList /> {' '}Suggestions</Link>
+          <Link className="btn btn-info btn-lg btn-suggest" to="#"><FaThList /> Suggestions</Link>
 
         </div>
       </div>
