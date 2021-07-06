2c31f0410b2c1bd2c65a93396e96da7d0bb4f83c
ashiviraji <46102737+ashiviraji@users.noreply.github.com>
2021-07-06 17:25:28 +0530

    Add delete button

--- src/components/Customer/billPlans.jsx
+++ src/components/Customer/billPlans.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import "../../assets/css/Customer/billPlans.css";
 import img1 from "../../assets/img/bill1.jpg";
 // import { Link } from "react-router-dom";
+import {RiDeleteBinFill} from 'react-icons/ri';
 const billPlans = () => {
     const cardDetails = [
         { Bill_Title: "Bill Plan 1", Model: "TOU", Total_amount: "LKR : 3500" },
@@ -28,8 +29,14 @@ const billPlans = () => {
                     </div>
 
                     <div className="view-more">
-                        View More
+                        More Details
                     </div>
+
+                    <div className="delete-plan">
+                       <RiDeleteBinFill></RiDeleteBinFill> Delete Plan
+                    </div>
+
+                    
                 </div>
             </div>
         </div>
