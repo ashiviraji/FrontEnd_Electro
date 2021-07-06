dce39f023dfab0aa596096d884ae88e91a33d243
hasinihatharasinghe97 <hasinividushanka@gmail.com>
2021-07-07 01:20:50 +0530

    Admin Update Unit Charges

---
+++ src/views/Admin/AdminUnitCharges0to60.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import AdminUnitLessthan60 from "../../components/Admin/AdminUnitCharges0to60";
+export default function AdminUnitCharges0to60() {
+  return (
+    <div>
+      <AdminUnitLessthan60 />
+    </div>
+  );
+}
