042aec3400c981d0cdd060af5dbb34ccb62d9643
TharinduWithanage <56493297+TharinduWithanage@users.noreply.github.com>
2021-07-07 01:40:01 +0530

    add table for calculating bill function

--- src/views/customer/CalculateBill.js
+++ src/views/customer/CalculateBill.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CalculateBillForm from './CalculateBillForm';
 import { Paper, makeStyles } from '@material-ui/core';
+import useTable from '../../components/Customer/useTable';
+import * as DeviceBill from './DeviceBill';
+import { TableBody } from '@material-ui/core'
+import { TableCell } from '@material-ui/core';
+import { TableRow } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
     pageContent : {
@@ -9,16 +14,42 @@ const useStyles = makeStyles((theme) => ({
     }
   }));
 
+const headCells = [
+    {id:'appliance', label:'Appliance'},
+    {id:'quantity', label:'Quantity'},
+    {id:'power', label:'Power'},
+    {id:'priority', label:'Priority'}
+]
+
 export default function CalculateBill() {
 
     const classes = useStyles();
+    const [records, setRecords] = useState(DeviceBill.getAllDevices())
     
+    const {
+        TblContainer,
+        TblHead
+    } = useTable(records,headCells);
 
     return (
         <div>
 
             <Paper className = {classes.pageContent}>
                 <CalculateBillForm/>
+                <TblContainer>
+                    <TblHead/>
+                    <TableBody>
+                        {
+                            records.map(item =>
+                                (<TableRow key={item.id}>
+                                    <TableCell>{item.appliance}</TableCell>
+                                    <TableCell>{item.quantity}</TableCell>
+                                    <TableCell>{item.power}</TableCell>
+                                    <TableCell>{item.priority}</TableCell>
+                                </TableRow>))
+                        }
+                    </TableBody>
+                </TblContainer>
             </Paper>
             
         </div>
