9362669073e91162d9b9c9739900da7ee5856716
ashiviraji <46102737+ashiviraji@users.noreply.github.com>
2021-07-06 17:12:59 +0530

    My Bill Plans

--- src/components/Customer/billPlans.jsx
+++ src/components/Customer/billPlans.jsx
@@ -1,13 +1,53 @@
 import React from 'react';
+
 import "../../assets/css/Customer/billPlans.css";
+import img1 from "../../assets/img/bill1.jpg";
+// import { Link } from "react-router-dom";
 const billPlans = () => {
-   return(
+    const cardDetails = [
+        { Bill_Title: "Bill Plan 1", Model: "TOU", Total_amount: "LKR : 3500" },
+        { Bill_Title: "Bill Plan 2", Model: "Fixed ", Total_amount: "LKR : 2500" },
+        { Bill_Title: "Bill Plan 3", Model: "Fixed ", Total_amount: "LKR : 4500" },
+
+    ];
+
+    return (
+          
+      cardDetails.map((card) => (
+        <div className="col-md-4">
         <div>
-              <div id="bill-plans-title-heading">
-                <label>My bill Plans</label>
+            <div className="card text-center bill-card-size">
+                <div className="overFlow">
+                    <img src={img1} alt="Image1" className="card-img-top" />
+                </div>
+                <div className="card-body text-dark bill-card" >
+                    <h4 className="card-title">{card.Bill_Title}</h4>
+                    <div className="bill-details">
+                        <label>Suitable Model &nbsp;: &nbsp; {card.Model} </label>
+                        <label>Total Amount &nbsp;: &nbsp; {card.Total_amount} </label>
+                    </div>
+
+                    <div className="view-more">
+                        View More
+                    </div>
+                </div>
             </div>
-           </div>
-   );
-}
+        </div>
+        </div>
+      )
+          
+           
+
+           
+
+
+
+
+
+ )
+    ) 
+    }
+
+
 
 export default billPlans;
