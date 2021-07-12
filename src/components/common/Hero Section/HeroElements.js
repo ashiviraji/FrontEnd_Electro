a44e8ae285a8fa53e32ddf2fc2d5dd9dce2e3c04
hasinihatharasinghe97 <hasinividushanka@gmail.com>
2021-07-12 17:05:50 +0530

    CSS changes done

--- src/components/common/Hero Section/HeroElements.js
+++ src/components/common/Hero Section/HeroElements.js
@@ -1,14 +1,14 @@
-import styled from 'styled-components';
-import { Link as LinkR } from 'react-router-dom';
-import { MdArrowForward, MdKeyboardArrowRight } from 'react-icons/md'
+import styled from "styled-components";
+import { Link as LinkR } from "react-router-dom";
+import { MdArrowForward, MdKeyboardArrowRight } from "react-icons/md";
 
 export const HeroContainer = styled.div`
-background:#0c0c0c;
+background:#0c0c0c; 
 display: flex;
 justify-content: center;
 align-items: center;
 padding: 0 30px;
-height:700px;
+height:750px;
 position: relative;
 z-index: 1;
 :before{
@@ -21,110 +21,96 @@ bottom:0;
 background:linear-gradient(180deg,rgba(0,0,0,0.2) 0%, rgba(0,0,0,0.6) 100%),
 linear-gradient(180deg,rgba(0,0,0,0.2) 0% ,transparent 100%) ;
 z-index: 2;
-`
+`;
 export const HeroBg = styled.div`
-position: absolute;
-top: 0;
-right: 0;
-bottom: 0;
-left: 0;
-width: 100%;
-height: 100%;
-overflow: hidden;
-`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+`;
 
 export const VideoBg = styled.video`
-width: 100%;
-height: 100%;
--o-object-fit:cover;
-object-fit: cover;
-background: #232a34;
-
-`
+  width: 100%;
+  height: 100%;
+  -o-object-fit: cover;
+  object-fit: cover;
+  background: #232a34;
+`;
 
 export const HeroContent = styled.div`
-z-index: 3;
-max-width: 1200px;
-position: absolute;
-padding: 8px 24px;
-display: flex;
-flex-direction: column;
-align-items: center;
-
-`
+  z-index: 3;
+  max-width: 1200px;
+  position: absolute;
+  padding: 8px 24px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 export const HeroH1 = styled.h1`
-color: #fff;
-font-size: 48px;
-text-align: center;
+  color: #fff;
+  font-size: 48px;
+  text-align: center;
 
-@media screen and (max-width: 818px){
-font-size: 32px;
+  @media screen and (max-width: 818px) {
+    font-size: 32px;
+  }
 
-}
-
-@media screen and(max-width: 480px){
-  margin-top: 24px;
-
-}
-
-`
+  @media screen and(max-width: 480px) {
+    margin-top: 24px;
+  }
+`;
 export const HeroP = styled.p`
-margin-top: 24px;
-color: #fff;
-font-size: 18px;
-text-align: center;
-max-width: 600px;
-@media screen and (max-width: 818px){
-font-size: 24px;
+  margin-top: 24px;
+  color: #fff;
+  font-size: 18px;
+  text-align: center;
+  max-width: 600px;
+  @media screen and (max-width: 818px) {
+    font-size: 24px;
+  }
 
-}
-
-@media screen and(max-width: 480px){
-  margin-top: 18px;
-
-}
-`
+  @media screen and(max-width: 480px) {
+    margin-top: 18px;
+  }
+`;
 export const HeroBtnWrapper = styled.div`
-
-margin-top: 32px;
-display: flex;
-flex-direction: column;
-align-items: center;
-
-`
+  margin-top: 32px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 export const ArrowForward = styled(MdArrowForward)`
-
-margin-left: 8px;
-font-size: 20px;
-
-`
+  margin-left: 8px;
+  font-size: 20px;
+`;
 export const ArrowRight = styled(MdKeyboardArrowRight)`
-margin-left: 8px;
-font-size: 20px;
-`
+  margin-left: 8px;
+  font-size: 20px;
+`;
 export const Button = styled(LinkR)`
+  outline: none;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  box-shadow: 5px 8px 5px #070d0d;
 
-outline: none;
-border: none;
-cursor: pointer;
-display: flex;
-justify-content: center;
-align-items: center;
-transition: all 0.2s ease-in-out;
-text-decoration: none;
- box-shadow: 5px 8px 5px #070D0D;
-
-&:hover{
-transition: all 0.2s ease-in-out;
-background:${({ primary }) => (primary ? '#fff' : '#01BF71')} ;
-color:#000;
-
-}
-font-size: ${({ fontBig }) => (fontBig ? '20px' : '16px')};
-color: ${({ dark }) => (dark ? '#010606' : '#fff')};
-white-space: nowrap;
-padding: ${({ big }) => (big ? '14px 48px' : '12px 30px')};
-border-radius: 50px;
-background:#08d37e ;
-
-`
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: ${({ primary }) => (primary ? "#fff" : "#01BF71")};
+    color: #000;
+  }
+  font-size: ${({ fontBig }) => (fontBig ? "20px" : "16px")};
+  color: ${({ dark }) => (dark ? "#010606" : "#fff")};
+  white-space: nowrap;
+  padding: ${({ big }) => (big ? "14px 48px" : "12px 30px")};
+  border-radius: 50px;
+  background: #08d37e;
+`;
