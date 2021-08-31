8edca3713409a02d45b00aebf9a012e5031574ca
ashiviraji <46102737+ashiviraji@users.noreply.github.com>
2021-08-31 15:40:03 +0530

    Special event tou

--- src/views/customer/SpecialTOUAddBill.js
+++ src/views/customer/SpecialTOUAddBill.js
@@ -204,6 +204,43 @@ export default function SpecialTOUAddBill() {
       }
     }
 
+    async function calculateSpecialEventTOUDevice() {
+
+      var token = document.cookie
+        .split(';')
+        .map(cookie => cookie.split('='))
+        .reduce((accumulator, [key, value]) => ({ ...accumulator, [key.trim()]: decodeURIComponent(value) }), {}).token;
+  
+  
+      var ParamsUserId = document.cookie
+        .split(';')
+        .map(cookie => cookie.split('='))
+        .reduce((accumulator, [key, value]) => ({ ...accumulator, [key.trim()]: decodeURIComponent(value) }), {}).userId;
+  
+  
+  
+      const response = await Axios.post(`${process.env.REACT_APP_BASE_URL}/calculate-special-event-TOUbill/${ParamsUserId}`, {
+  
+        bill_id: newBillId
+      }, {
+        headers: {
+          authorization: `Token ${token}`
+        }
+      })
+       console.log(response.data);
+      if (response.data.status) {
+        // setCalculatedData(response.data.data)
+  
+  
+      } else {
+        console.log(response.data.message);
+        history.push("/sign-in");
+        window.location.reload();//reload browser
+        deleteAllCookies();//delete all cookies
+      }
+  
+    }
+
     return (
         <div>
             
@@ -245,6 +282,7 @@ export default function SpecialTOUAddBill() {
               <TableCell style={{textAlign:"center"}}>Peak Hour</TableCell>
               <TableCell style={{textAlign:"center"}}>Off Peak Hour</TableCell>
               <TableCell style={{textAlign:"center"}}>Day Hour</TableCell>
+              <TableCell style={{textAlign:"center"}}>No Of Days</TableCell>
               <TableCell style={{textAlign:"center"}}>Actions</TableCell>
             </TableRow>
           </TableHead>
@@ -309,7 +347,7 @@ export default function SpecialTOUAddBill() {
                   />
                 </Col>
                 <Col sm="4">
-                <button type="button" className="btn btn-success calculate-button-special-event">
+                <button type="button" className="btn btn-success calculate-button-special-event" onClick={calculateSpecialEventTOUDevice}>
                     Calculate
                 </button>
                 </Col>
