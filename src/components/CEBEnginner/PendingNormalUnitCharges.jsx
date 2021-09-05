d7d87e5354b343848d3ec6033576c3ea7f760d69
MadhushankaUCSC <buthsaramadhushanka@gmail.com>
2021-09-06 01:21:09 +0530

    pending unit charges edit

--- src/components/CEBEnginner/PendingNormalUnitCharges.jsx
+++ src/components/CEBEnginner/PendingNormalUnitCharges.jsx
@@ -13,7 +13,11 @@ import { EditOutlined } from "@material-ui/icons";
 import UseTable from "./UseTable";
 import ConfirmationBox from "../common/ConfirmationBox";
 import ConfirmDialog from "../Customer/bill_control/ConfirmDialog";
-
+import { Modal, Button } from "react-bootstrap";
+import "../../assets/css/CEBEngineer/engineerpopup.css";
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+toast.configure();
 const headCells = [
   { id: "Unit_charges_requested_date", label: "Requested Date" },
   { id: "Unit_category", label: "Unit Category" },
@@ -37,8 +41,15 @@ const useStyles = makeStyles({
   },
 });
 
-const PendingNormalUnitCharges = ({ setVisibleState1 }) => {
+export default function PendingNormalUnitCharges({ setVisibleState1 }) {
 
+  const [modalShow, setModalShow] = React.useState(false);
+
+  const [normalUCharge, setNormalUCharge] = useState("");
+  const [normalReqUCharge, setNormalReqUCharge] = useState("");
+
+  const [normalUnitPeriod, setNormalUnitPeriod] = useState("");
+  const [Category, setCategory] = useState("");
 
   const classes = useStyles();
   const [records, setDeviceData] = useState([]);
@@ -141,6 +152,16 @@ const PendingNormalUnitCharges = ({ setVisibleState1 }) => {
     getPendingUnit();
   }, []);
 
+  function setDataToPopup(value, unitPeriod, requestValue, categ) {
+    setModalShow(true);
+    setNormalUCharge(value);
+    setNormalReqUCharge(requestValue);
+    setNormalUnitPeriod(unitPeriod);
+    setCategory(categ)
+
+  }
+
+
   const onDeletedevice = async (unitPeriod, categoryName) => {
 
     setConfirmDialog({
@@ -209,14 +230,13 @@ const PendingNormalUnitCharges = ({ setVisibleState1 }) => {
               <TableCell>
                 <button
                   className="btn editActionButtonIcon" style={{ cursor: "pointer" }}
-                // onClick={() => {
-                //   openInPopup(item);
-                // }}
+                  onClick={() => setDataToPopup(item.Unit_charge, item.Unit_category, item.Update_unit_charges, "Unit")}
                 >
                   <EditOutlined
                     fontSize="small"
                     ClassName={classes.actionButtonIcon}
                   />
+
                 </button>
                 <button
                   className="btn deleteActionButtonIcon"
@@ -239,7 +259,21 @@ const PendingNormalUnitCharges = ({ setVisibleState1 }) => {
                   />
                 </button>
               </TableCell>
+
+              <MyVerticallyCenteredModal
+                unitPrice={normalUCharge}
+                reqUnitPrice={normalReqUCharge}
+                unitPeriod={normalUnitPeriod}
+                categoryName={Category}
+                show={modalShow}
+                getFunc={getPendingUnit}
+                onHide={() => setModalShow(false)}
+              />
+
             </TableRow>
+
+
+
           ))}
         </TableBody>
       </TblContainer>
@@ -257,6 +291,195 @@ const PendingNormalUnitCharges = ({ setVisibleState1 }) => {
 
     </Paper>
   );
-};
+}
 
-export default PendingNormalUnitCharges;
+
+function MyVerticallyCenteredModal(props) {
+
+  const [NewAmount, setNewAmount] = useState("");
+
+  let history = useHistory();
+  const [confirmDialog, setConfirmDialog] = useState({
+    isOpen: false,
+    title: "",
+    subTitle: "",
+  });
+  const [confirmationBox, setConfirmationBox] = useState({
+    isOpen: false,
+    title: "",
+    subTitle: "",
+  });
+
+  function updateData(e) {
+    e.preventDefault();
+
+    getUpdatedata(e);
+    props.onHide();
+  }
+
+
+  function getUpdatedata(e) {
+    // console.log(NewAmount, props.categoryName, props.timePeriod);
+    e.preventDefault();
+    setConfirmDialog({
+      ...confirmDialog,
+      isOpen: false,
+    });
+    setConfirmationBox({
+      ...confirmationBox,
+      isOpen: false,
+    });
+    var token = document.cookie
+      .split(';')
+      .map(cookie => cookie.split('='))
+      .reduce((accumulator, [key, value]) => ({ ...accumulator, [key.trim()]: decodeURIComponent(value) }), {}).token;
+
+    var categoryId = "normal";
+
+
+
+    Axios.post(`${process.env.REACT_APP_BASE_URL}/unit-charges-update/${categoryId}`, {
+      newPrice: NewAmount,
+      categoryName: props.categoryName,
+      unitPeriod: props.unitPeriod
+    }, {
+      headers: {
+        authorization: `Token ${token}`
+      }
+    })
+      .then((response) => {
+        // console.log(response.data.data[1]);
+
+        if (response.data.status) {
+          // window.location.reload();//reload browser
+          props.getFunc();
+          toast.success('Edit Successfuly', {
+            autoClose: 7000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
+        } else {
+
+          confirmation()
+        }
+      }).catch((error) => {
+        console.log("this is error  response", error);
+      });
+  }
+
+  /**
+        * function of delete all cookies
+        */
+  function deleteAllCookies() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = cookies[i];
+      var eqPos = cookie.indexOf("=");
+      var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+  }
+
+
+
+  function confirmation() {
+    setConfirmationBox({
+      isOpen: true,
+      title: "Can Not Perform This Action!",
+      subTitle: "Your session has timed out. Please log in again.",
+      btnStatus: "warning",
+      onConfirm: () => {
+        history.push("/sign-in");
+        window.location.reload();//reload browser
+        deleteAllCookies();
+      },
+    });
+  }
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <form >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Update {props.categoryName} Charges - {props.unitPeriod}
+          </Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <div
+            className="engineer-popup-price-changes"
+            style={{ display: "flex" }}
+          >
+            <h4>Current {props.categoryName} Price </h4>
+
+            <label className="engineer-current-label">LKR : {props.unitPrice}</label>
+
+          </div>
+          <div
+            className="engineer-popup-price-changes"
+            style={{ display: "flex" }}
+          >
+            <h4>Requested {props.categoryName} Price </h4>
+
+            <label className="engineer-current-label">LKR : {props.reqUnitPrice}</label>
+
+          </div>
+          {/* <div
+          className="engineer-popup-price-changes"
+          style={{ display: "flex" }}
+        >
+          <h4>Increasing Amount </h4>
+          <input className="engineer-increase-amount"></input>
+        </div> */}
+
+          <div
+            className="engineer-popup-price-changes"
+            style={{ display: "flex" }}
+          >
+            <h4>New {props.categoryName} Price </h4>
+
+            <input className="engineer-new-unit-price" onChange={(e) => { setNewAmount(e.target.value); }} placeholder="LKR" required></input>
+          </div>
+        </Modal.Body>
+
+        <Modal.Footer id="engineer-accept-reject-button">
+          <Button type="button" className="engineer-UpdateButton" onClick={(e) => {
+            setConfirmDialog({
+              isOpen: true,
+              title: "Are You Sure Make Changes",
+              subTitle: `Current Price: ${props.unitPrice} New Price: ${NewAmount}`,
+              btnStatus: "success",
+              onConfirm: (e) => {
+                updateData(e);
+              },
+            })
+
+          }}>
+            UPDATE
+          </Button>
+          <Button onClick={props.onHide} className="engineer-CancelButton">
+            CANCEL
+          </Button>
+        </Modal.Footer>
+      </form>
+      <ConfirmDialog
+        confirmDialog={confirmDialog}
+        setConfirmDialog={setConfirmDialog}
+      />
+      <ConfirmationBox
+        confirmationBox={confirmationBox}
+        setConfirmationBox={setConfirmationBox}
+      />
+    </Modal>
+  );
+}
+
+// export default PendingNormalUnitCharges;
