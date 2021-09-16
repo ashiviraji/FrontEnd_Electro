e9a21cea43ff193035f51b6701fe414fe536018e
Minuri427 <yasarawickramanayaka@gmail.com>
2021-09-16 17:49:51 +0530

    get device wise details from backend

--- src/components/Customer/deviceChartFixed.jsx
+++ src/components/Customer/deviceChartFixed.jsx
@@ -1,8 +1,52 @@
-import React from "react";
+import {React, useState, useEffect } from "react";
 import "../../assets/css/Customer/deviceChartfixed.css";
 import { Pie } from "react-chartjs-2";
+import Axios from 'axios';
 
 export default function DeviceChartfixed() {
+  const params = new URLSearchParams(window.location.search)
+  const calculatedBillId  = params.get('bill_id');
+
+  const [chartData,setChartData] = useState([]);
+
+
+  async function getDeviceDetailsFixed(newBillId) {
+
+    var ParamsUserId = document.cookie
+      .split(';')
+      .map(cookie => cookie.split('='))
+      .reduce((accumulator, [key, value]) => ({ ...accumulator, [key.trim()]: decodeURIComponent(value) }), {}).userId;
+  
+  
+    var token = document.cookie
+      .split(';')
+      .map(cookie => cookie.split('='))
+      .reduce((accumulator, [key, value]) => ({ ...accumulator, [key.trim()]: decodeURIComponent(value) }), {}).token;
+  
+  
+      // let History = useHistory();
+      console.log("call device detail fixed function")
+  
+      const response = await Axios.post(`${process.env.REACT_APP_BASE_URL}/get-device-wise-usage-fixed-main/${ParamsUserId}`, {
+          newBillId: newBillId
+      }, {
+          headers: {
+              authorization: `Token ${token}`
+          }
+      })
+  
+      console.log(response.data.data);
+      return response.data.data;
+  
+  }
+  
+  
+    useEffect( async () => {
+  
+      var devices_data_fixed = await getDeviceDetailsFixed(calculatedBillId);
+      setChartData(devices_data_fixed);
+    },[]);
+
   return (
     <div>
       <h4 className="MainTitle-fixed text-center">
