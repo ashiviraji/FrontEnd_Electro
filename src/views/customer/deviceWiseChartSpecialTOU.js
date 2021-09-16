4c90660aa0e3e3e93c9c414c9a8cb4be55adacde
Minuri427 <yasarawickramanayaka@gmail.com>
2021-09-16 16:38:10 +0530

    create view for special event tou chart

---
+++ src/views/customer/deviceWiseChartSpecialTOU.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+import DeviceChartSpTOU from "../../components/Customer/DeviceChartSpTOU";
+
+
+export default function devicewiseChart() {
+  return (
+    <div>
+      <DeviceChartSpTOU />
+    </div>
+  );
+}
+
