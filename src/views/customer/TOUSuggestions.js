b14743bcb61026ac0950c3e135ed3bac4fbe85f4
Ashika Abeysuriya <ashikaabeysuriya456@gmail.com>
2021-09-17 15:32:18 +0530

    TOUSuggestions.js

--- src/views/customer/TOUSuggestions.js
+++ src/views/customer/TOUSuggestions.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import '../../assets/css/TOUSuggestions.css';
-import {BsSearch} from 'react-icons/bs';
-import {IoMdDownload} from "react-icons/io";
 
-
+//import {IoMdDownload} from "react-icons/io";
 import TOUSuggestionsPage from './../../components/Customer/TOUSuggestions';
 
 export default function TOUSuggestions() {
@@ -14,19 +12,8 @@ export default function TOUSuggestions() {
       <div className="device-wise-title-TOU">
        TOU SUGGESTIONS
     </div>
-    
-      
-       
-      
-      
-   
     <input  className="download-button" type="Button" value="Download As Pdf"  />
-    <div className="ui search">
-      <div className="ui icon1 input">
-    <input  className="search-bar" type="Text" placeholder="Search Device" />
-    <BsSearch className="bsSearch"></BsSearch>
-    </div>
-    </div>
+    
    
     <TOUSuggestionsPage />
     </div>
