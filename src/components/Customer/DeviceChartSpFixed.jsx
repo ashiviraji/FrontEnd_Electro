4743da6a07949c26ad87167dcb2ce60753540322
Minuri427 <yasarawickramanayaka@gmail.com>
2021-09-18 22:25:54 +0530

    add bar chart to sp event fixed

--- src/components/Customer/DeviceChartSpFixed.jsx
+++ src/components/Customer/DeviceChartSpFixed.jsx
@@ -1,17 +1,18 @@
 import { React, useState, useEffect } from "react";
 import "../../assets/css/Customer/deviceChartfixed.css";
-import { Pie } from "react-chartjs-2";
+import { Pie, Bar } from "react-chartjs-2";
 import Axios from "axios";
 
 export default function DeviceChartfixed() {
   
   const params = new URLSearchParams(window.location.search)
   const BillId  = params.get('bill_id');
-  console.log("+++++"+BillId);
 
   const [appliance, setAppliance] = useState([]);
   const [units, setUnits] = useState([]);
   const [colors, setColors] = useState([]);
+  const [max,setMax] = useState([]);
+  const [maxApp,getMaxApp] = useState("");
 
   async function getSpecialEventDeviceDetailsFixed(newBillId) {
 
@@ -64,16 +65,37 @@ export default function DeviceChartfixed() {
       unitList.push(chartData[i].	total_units_fixed);
       colorList.push(generateColor());
     }
+    
+    var maxunit = chartData[0].total_units
+
+    for(i=0;i< chartData.length; i++){
+      if(chartData[i].total_units>maxunit){
+        maxunit = chartData[i].total_units;
+      }
+    }
 
     setAppliance(applianceList);
     setUnits(unitList);
     setColors(colorList);
+    setMax(maxunit);
   }
+
+  function getMaxAppliace(chartData,max){
+    var max_appliance;
+    for(var x=0; x< chartData.length; x++ ){
+      if(chartData[x].total_units===max){
+        max_appliance = chartData[x].appliance;
+      }
+    }
+    getMaxApp(max_appliance);
+  }
+
    
   useEffect( async () => {
   
     var special_event_data_fixed = await getSpecialEventDeviceDetailsFixed(BillId);
     await getSpData(special_event_data_fixed);
+    await getMaxAppliace(special_event_data_fixed,max);
   },[]);
 
   return (
@@ -96,6 +118,38 @@ export default function DeviceChartfixed() {
                           datasets: [
                             {
                               data:units,
+                              backgroundColor: colors,
+                              hoverOffset: 4,
+                            },
+                          ],
+                        }}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row row-fixed">
+        <div class="col-sm-6">
+          <div class="card">
+            <div class="card-body">
+              <h6 class="card-title text-center">
+                Unit Usage - Device Wise (kWh)
+              </h6>
+              <div class="col-sm-12">
+                <div class="card chart-fixed">
+                  <div class="card-body">
+                    <div className="chart-devicewise">
+                      <Bar
+                        data={{
+                          labels: appliance,
+                          datasets: [
+                            {
+                              label: maxApp,
+                              data: units,
                               backgroundColor: colors,
                               hoverOffset: 4,
                             },
