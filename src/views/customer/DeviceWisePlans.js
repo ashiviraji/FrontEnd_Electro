ef355489bc19d836bf9c748c9fa745c849d040bd
Minuri427 <yasarawickramanayaka@gmail.com>
2021-09-19 23:59:19 +0530

    breadcrumb for bill blan pages

--- src/views/customer/DeviceWisePlans.js
+++ src/views/customer/DeviceWisePlans.js
@@ -1,26 +1,39 @@
 import React from "react";
 import "../../assets/css/Customer/billPlans.css";
 import { RiFileAddLine } from "react-icons/ri";
-import BillPlansPage from "./../../components/Customer/billPlans";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 
 import DeviceWisePlanCard from "../../components/Customer/DeviceWisePlans";
 import "../../assets/css/Customer/deviewisePlans.css";
-import { Link } from "react-router-dom";
+import { Link as Link1} from "react-router-dom";
 
 export default function DeviceWisePlans() {
   return (
     <div>
+      <Breadcrumbs
+        aria-label="breadcrumb"
+        style={{ marginTop: "2rem", marginLeft: "2rem" }}
+        separator={<NavigateNextIcon fontSize="small" />}
+      >
+        <Link underline="hover" color="blue" href="/dashboard-user">
+          Dashboard
+        </Link>
+        <Typography color="text.primary"> My Bill Plans</Typography>
+      </Breadcrumbs>
       <div id="bill-plans-title-heading">
         <label>MY BILL PLANS</label>
       </div>
-      <Link to="/manage-bill">
+      <Link1 to="/manage-bill">
         <button className="add-new-bill-plan">
           <RiFileAddLine
             style={{ width: "25px", height: "25px" }}
           ></RiFileAddLine>
           <label className="new-bill-text">Add New Bill Plan</label>
         </button>
-      </Link>
+      </Link1>
       
       <DeviceWisePlanCard />
     </div>
