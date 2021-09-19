20ca1b9a80b5e849e8a27ad4c5005382f645c614
TharinduWithanage <56493297+TharinduWithanage@users.noreply.github.com>
2021-09-19 23:39:36 +0530

    add breadcrumb

--- src/views/customer/DeviceWiseTOUSeperate.js
+++ src/views/customer/DeviceWiseTOUSeperate.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Typography from '@mui/material/Typography';
+import Link_ from '@mui/material/Link';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 
 import TOUDeviceWise  from "../../components/Customer/TOUDeviceWiseUsage";
 
 export default function DeviceWiseFixedUsageSeprate() {
+
+  const params = new URLSearchParams(window.location.search)
+  const calculatedBillId  = params.get('bill_id');
   
   return( 
   <div className="device-wise-usage">
+    <Breadcrumbs aria-label="breadcrumb" style={{marginTop: '2rem',marginLeft: '2rem'}} separator={<NavigateNextIcon fontSize="small" />}>
+        <Link_ underline="hover" color="blue" href="/my-bill-plans">
+          My Bill Plans
+        </Link_ >
+        <Link_ underline="hover" color="blue" href={`/bill-comparison?bill_id=${calculatedBillId}`}>
+          More Details
+        </Link_ >
+        <Typography color="text.primary">Suggestions</Typography>
+      </Breadcrumbs>
   <div className="title-heading">
-     <label className="tou-headline">TIME OF USE USAGE</label>
+     <label className="tou-headline"><b>TIME OF USE USAGE</b></label>
       
   </div>
  
