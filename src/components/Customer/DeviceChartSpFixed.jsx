410dc377c404cb6c960e3c866241322f14fd9f43
Minuri427 <yasarawickramanayaka@gmail.com>
2021-09-19 08:23:29 +0530

    chart view modifications

--- src/components/Customer/DeviceChartSpFixed.jsx
+++ src/components/Customer/DeviceChartSpFixed.jsx
@@ -11,8 +11,8 @@ export default function DeviceChartfixed() {
   const [appliance, setAppliance] = useState([]);
   const [units, setUnits] = useState([]);
   const [colors, setColors] = useState([]);
-  const [max,setMax] = useState([]);
-  const [maxApp,getMaxApp] = useState("");
+  const [max,setMax] = useState(0);
+  const [maxApp,setMaxApp] = useState("");
 
   async function getSpecialEventDeviceDetailsFixed(newBillId) {
 
@@ -26,11 +26,7 @@ export default function DeviceChartfixed() {
       .split(';')
       .map(cookie => cookie.split('='))
       .reduce((accumulator, [key, value]) => ({ ...accumulator, [key.trim()]: decodeURIComponent(value) }), {}).token;
-  
-  
-      // let History = useHistory();
-      console.log("call special event device detail fixed function")
-  
+    
       const response = await Axios.post(`${process.env.REACT_APP_BASE_URL}/get-specialEvent-details-devicewise-fixed/${ParamsUserId}`, {
           newBillId: newBillId
       }, {
@@ -39,7 +35,6 @@ export default function DeviceChartfixed() {
           }
       })
   
-      console.log(response.data.data);
       return response.data.data;
   
   }
@@ -54,40 +49,42 @@ export default function DeviceChartfixed() {
     return color;
   }
 
-  function getSpData(chartData) {
+  async function getSpData(chartData) {
     var i;
     var applianceList = [];
     let unitList = [];
     let colorList = [];
 
-    for (i = 0; i < 2; i++) {
+    for (i = 0; i < chartData.length; i++) {
       applianceList.push(chartData[i].appliance);
       unitList.push(chartData[i].	total_units_fixed);
       colorList.push(generateColor());
     }
     
-    var maxunit = chartData[0].total_units
+    var maxunit = chartData[0].total_units_fixed;
 
     for(i=0;i< chartData.length; i++){
-      if(chartData[i].total_units>maxunit){
-        maxunit = chartData[i].total_units;
+      if(chartData[i].total_units_fixed>maxunit){
+        maxunit = chartData[i].total_units_fixed;
       }
+
     }
 
-    setAppliance(applianceList);
-    setUnits(unitList);
-    setColors(colorList);
-    setMax(maxunit);
+    await setAppliance(applianceList);
+    await setUnits(unitList);
+    await setColors(colorList);
+    await setMax(maxunit);
   }
 
   function getMaxAppliace(chartData,max){
+   
     var max_appliance;
     for(var x=0; x< chartData.length; x++ ){
-      if(chartData[x].total_units===max){
+      if(chartData[x].total_units_fixed==max){
         max_appliance = chartData[x].appliance;
       }
     }
-    getMaxApp(max_appliance);
+    setMaxApp(max_appliance);
   }
 
    
@@ -95,9 +92,11 @@ export default function DeviceChartfixed() {
   
     var special_event_data_fixed = await getSpecialEventDeviceDetailsFixed(BillId);
     await getSpData(special_event_data_fixed);
+    console.log("________"+max);
     await getMaxAppliace(special_event_data_fixed,max);
   },[]);
 
+ 
   return (
     <div>
       <h4 className="MainTitle-fixed text-center">
@@ -148,13 +147,15 @@ export default function DeviceChartfixed() {
                           labels: appliance,
                           datasets: [
                             {
-                              label: maxApp,
+                              axis: 'y',
+                              label: "Maximum Usage",
                               data: units,
                               backgroundColor: colors,
                               hoverOffset: 4,
                             },
                           ],
                         }}
+                        
                       />
                     </div>
                   </div>
