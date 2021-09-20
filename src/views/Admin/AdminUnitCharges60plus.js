51dc78e4df54d9d45c0d2312b2e85ad7ceaa2164
Minuri427 <yasarawickramanayaka@gmail.com>
2021-09-20 18:33:51 +0530

    added breadcrumb for admin pages

--- src/views/Admin/AdminUnitCharges60plus.js
+++ src/views/Admin/AdminUnitCharges60plus.js
@@ -1,9 +1,25 @@
 import React from "react";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+
 
 import AdminUnit60plus from "../../components/Admin/AdminUnitCharges60plus";
+
 export default function AdminUnitCharges60plus() {
   return (
     <div>
+      <Breadcrumbs
+        aria-label="breadcrumb"
+        style={{ marginTop: "2rem", marginLeft: "2rem" }}
+        separator={<NavigateNextIcon fontSize="small" />}
+      >
+        <Link underline="hover" color="blue" href="/admin-unit-charges">
+          Unit Charges
+        </Link>
+        <Typography color="text.primary">Unit Charges 60+</Typography>
+      </Breadcrumbs>
       <AdminUnit60plus />
     </div>
   );
--- src/views/Admin/UnitChargesAdminHome.js
+++ src/views/Admin/UnitChargesAdminHome.js
@@ -1,10 +1,25 @@
 import React from "react";
 import AdminUChargeHome from "../../components/Admin/AdminUnitChargeHome";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 
 export default function UnitChargesAdminHome() {
   return (
     <div>
-      < AdminUChargeHome/>
+      <Breadcrumbs
+        aria-label="breadcrumb"
+        style={{ marginTop: "2rem", marginLeft: "2rem" }}
+        separator={<NavigateNextIcon fontSize="small" />}
+      >
+        <Link underline="hover" color="blue" href="/dashboard-admin">
+          Dashboard
+        </Link>
+        <Typography color="text.primary">Unit Charges</Typography>
+      </Breadcrumbs>
+
+      <AdminUChargeHome />
     </div>
   );
 }
